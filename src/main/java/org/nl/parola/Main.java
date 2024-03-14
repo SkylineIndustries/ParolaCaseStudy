b5f7869bc4a862fc91784cd8914cd809b91b0a0f
TheYeetius <TJR.Droppert@student.han.nl>
2024-03-15 00:10:16 +0100

    Opzetten omgeving taak

--- src/main/java/org/nl/parola/Main.java
+++ src/main/java/org/nl/parola/Main.java
@@ -1,16 +1,11 @@
 package org.nl.parola;
 
-import org.nl.parola.quiz.Quiz;
-import org.nl.parola.testcode.IScoreBerekening;
-import org.nl.parola.testcode.ScoreStrategyA;
-import org.nl.parola.testcode.ScoreStrategyB;
+
+import org.nl.parola.parola.Parola;
 
 public class Main {
     public static void main(String[] args) {
-        Quiz quiz = new Quiz();
-        ScoreStrategyA scoreStrategyA = new ScoreStrategyA();
-        ScoreStrategyB scoreStrategyB = new ScoreStrategyB();
-        int score = quiz.berekenScore(scoreStrategyB);
-        System.out.println(score);
+        Parola parola = new Parola(null, null);
+
     }
 }
